cb15cc2a305e787ec0f811457c484c47ba8f6204
digstm <60488940+digstm@users.noreply.github.com>
2020-06-02 14:40:25 +1200

    Create greeting.php

---
+++ php-intro/greeting.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <a href="name.php">bak</a>
+    <?php
+
+    $num = 1;
+    $top_num = 
+
+    while ($num <= $top_num) {
+    $num_divied = $num % 3;
+    if ($num_divied == 0) {
+      echo "<p>fizbuzz </p>";
+    } else {
+      echo $num;
+    }
+    $num = $num + 1;
+    }
+     ?>
+
+  </body>
+</html>
