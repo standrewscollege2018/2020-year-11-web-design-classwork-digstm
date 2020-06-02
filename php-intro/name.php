3009c065a438fb8b262ca1ee36f80f9a8c4b873f
digstm <60488940+digstm@users.noreply.github.com>
2020-06-02 14:40:31 +1200

    Create name.php

---
+++ php-intro/name.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="greeting.php" method="post">
+      name : <input type="text" name="top_num" value="">
+      <button type="submit" name="button">go</button>
+
+    </form>
+  </body>
+</html>
