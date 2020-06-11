bc8df58d89c71f913bd3549ba25c57d0be7f25d2
digstm <60488940+digstm@users.noreply.github.com>
2020-06-12 10:03:45 +1200

    Create chouse.php

---
+++ php-intro/chouse.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>testing-get-ara</title>
+  </head>
+  <body>
+
+    <!-- intrushons will go below. -->
+    <p>Clike the like below</p>
+    <a href="respond.php?number=1">go</a>
+    <a href="respond.php?number=2">go2</a>
+
+  </body>
+</html>
