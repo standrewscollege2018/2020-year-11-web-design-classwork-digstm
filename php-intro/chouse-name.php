1a159e6cc5c2497e8998f4b3dab262d9a3f366b0
digstm <60488940+digstm@users.noreply.github.com>
2020-06-12 10:03:41 +1200

    Create chouse-name.php

---
+++ php-intro/chouse-name.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="form.php" method="post">
+      name : <input type="text" name="name-chosen" value="">
+      <button type="submit" name="button">go</button>
+      </form>
+  </body>
+</html>
